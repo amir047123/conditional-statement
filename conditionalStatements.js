eb09a0acde0f720afbdf8e31d345e4190a2dc8c0
amir047123 <amirfaysal047123@gmail.com>
2025-02-06 22:04:59 +0600

    done

---
+++ conditionalStatements.js
@@ -0,0 +1,94 @@
+//if statement
+
+// let age = 18;
+
+// if (age >= 18) {
+//   console.log("you can vote");
+// }
+
+//if else statement
+
+// let age = 16;
+
+// if (age >= 18) {
+//   console.log("you can vote");
+// } else {
+//   console.log("you can't vote");
+// }
+
+// if else if statement
+
+// let marks = 60;
+
+// if (marks >= 80) {
+//   console.log("A+ grade");
+// } else if (marks >= 70) {
+//   console.log("A grade ");
+// } else if (marks >= 60) {
+//   console.log("A- grade");
+// } else {
+//   console.log("fail");
+// }
+
+// switch case statement
+
+// let day = 5;
+
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+
+//   case 4:
+//     console.log("thursday");
+//     break;
+
+//   case 5:
+//     console.log("friday");
+//     break;
+
+//   default:
+//     console.log("invalid day");
+//     break;
+// }
+
+// ternary operator
+
+// condition ? expression_if_true : expression_if_false;
+
+// let age = 16;
+
+// let message = age >= 18 ? "you can vote" : "you can't vote";
+
+// console.log(message);
+
+// js loop 3
+
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+
+// for loop
+
+// for (initialization; condition; increment / decrement) {
+//   // loop body
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// while loop
+
+// let i = 1;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
